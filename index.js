b77a4dc8b198725b52fffff6e55cf53fae62c614
pitiao145 <pierre.bruyninckx1@gmail.com>
2024-07-18 15:44:56 +0800

    Full funcionality added and refactored. Next up: personalising

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ import { getDatabase, ref, push, onValue, remove, get, update } from "https://ww
 const appSettings = {
     databaseURL: "https://we-are-the-champions-39285-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
-
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-
 const endorsementsInDB = ref(database, "endorsements")
 
-
+// Assign DOM variables
 const endorsementInput = document.getElementById("endorsement-input")
 const fromInput = document.getElementById("from-input")
 const toInput = document.getElementById("to-input")
 const publishBtn = document.getElementById("publish-btn")
 const endorsements = document.getElementById("endorsements")
-// Store it and send it to the database
-// Everytime there's a change to the database, get all the elements in it
-// and display them on the page
-
-//To create an endorsement object
+// Functions
+//Define an endorsement object
 function Endorsement(text, from, to) {
     this.text = text;
     this.from = from;
@@ -29,20 +24,24 @@ function Endorsement(text, from, to) {
     this.likes = 0;
 }
 
+// Sends an endorsement object to the DB
 function sendToDb(data) {
     push(endorsementsInDB, data)
 }
 
+// Clears all the inputs
 function clearInputs() {
     endorsementInput.value = ""
     fromInput.value = ""
     toInput.value = ""
 }
 
+// Clears the endorsement field
 function clearEndorsements() {
     endorsements.innerHTML = ""
 }
 
+// Grabs the inputs from the users and send them to the DB
 function publish() {
     //Get endorsement data
     const endorsementText = endorsementInput.value
@@ -53,24 +52,44 @@ function publish() {
     clearInputs()
 }
 
-function createEndorsement(id, from, to, text, likes) {
+function createEndorsementBox(id) {
     const endorsementBox = document.createElement("div")
     endorsementBox.className = "endorsement-box"
     endorsementBox.id = id
-    endorsementBox.addEventListener('dblclick', function() {
-        addLike(id)
+    endorsementBox.addEventListener('dblclick', function () {
+        handleLike(id)
     })
+    return endorsementBox
+}
+
+function createEndorsementText(text) {
     const endorsementText = document.createElement("p")
     endorsementText.textContent = text
-    const toSpan = document.createElement("span")
-    const fromSpan = document.createElement("span")
-    const hearts = document.createElement("span")
-    toSpan.textContent = "To " + to
-    fromSpan.textContent = "From " + from
-    hearts.textContent = "❤ " + likes
-    toSpan.className = "to-span"
-    fromSpan.className = "from-span"
-    hearts.className = "hearts"
+    return endorsementText
+}
+
+function createFromToHeartSpan(toFromHeart, name, likes) {
+    const span = document.createElement("span")
+    if (toFromHeart === "to") {
+        span.textContent = "To " + name
+        span.className = "to-span"
+    } else if (toFromHeart === "from") {
+        span.textContent = "From " + name
+        span.className = "from-span"
+    } else if (toFromHeart === "heart") {
+        span.textContent = "❤ " + likes
+        span.className = "hearts"
+    }
+    return span
+}
+
+// To create a visual for an endorsment pulled from DB.
+function createEndorsement(id, from, to, text, likes) {
+    const endorsementBox = createEndorsementBox(id)
+    const endorsementText = createEndorsementText(text)
+    const toSpan = createFromToHeartSpan("to", to)
+    const fromSpan = createFromToHeartSpan("from", from)
+    const hearts = createFromToHeartSpan("heart", to, likes)
     endorsementBox.appendChild(toSpan)
     endorsementBox.appendChild(endorsementText)
     endorsementBox.appendChild(fromSpan)
@@ -85,53 +104,34 @@ function listEndorsements(array) {
         const endorsementTo = array[i][1].to
         const endorsementText = array[i][1].text
         const endorsementLikes = array[i][1].likes
-        console.log("ID: " + endorsementID)
-        console.log("From: " + endorsementFrom)
-        console.log("To: " + endorsementTo)
-        console.log("Text: " + endorsementText)
-        console.log("Likes: " + endorsementLikes)
+        // console.log("ID: " + endorsementID)
+        // console.log("From: " + endorsementFrom)
+        // console.log("To: " + endorsementTo)
+        // console.log("Text: " + endorsementText)
+        // console.log("Likes: " + endorsementLikes)
         // console.log("Key: " + endorsementKey + "| Value: " + endorsementValue)
         createEndorsement(endorsementID, endorsementFrom, endorsementTo, endorsementText, endorsementLikes)
     }
 }
 
-onValue(endorsementsInDB, function(snapshot) {
-    if (snapshot.exists()) {
-        clearEndorsements()
-        let endorsementsArray = Object.entries(snapshot.val())
-        console.log(endorsementsArray)
-        listEndorsements(endorsementsArray)
-    } else {
-        const text = document.createElement("p")
-        text.innerText = "No endorsements here yet"
-        endorsements.append(text)
-    }
-})
-
-function addLike(id) {
+// Handles a double click on an endorsement depending on if it was already liked by the user or not
+function handleLike(id) {
     // Get the path to the required endorsement
     const path = `endorsements/${id}`
-    console.log(path)
     // Get the current amount of likes on the endorsement
-    var databaseItem = ref(database, path)
-    console.log("Ref: " + databaseItem)
+    const databaseItem = ref(database, path)
+    // console.log("Ref: " + databaseItem)
     get(databaseItem)
         .then((snapshot) => {
             if (snapshot.exists()) {
                 const currentLikes = snapshot.val().likes
-                console.log(currentLikes)
-                const newLikes = currentLikes + 1
-                // Update the like count
-                const updates = {likes: newLikes}
-                
-                update(databaseItem, updates)
-                    .then(() => {
-                        console.log("Data updated successfully.");
-                    })
-                    .catch((error) => {
-                        console.error("Error updating data: ", error);
-                    });
-            
+                if (localStorage.getItem(id) != "liked") {
+                    addLikeInDB(databaseItem, currentLikes)
+                    localStorage.setItem(id, "liked")
+                } else if (localStorage.getItem(id) === "liked") {
+                    removeLikeInDB(databaseItem, currentLikes)
+                    localStorage.setItem(id, "not liked")
+                }
             } else {
                 console.log("No data available for the likes");
             }
@@ -139,11 +139,48 @@ function addLike(id) {
         .catch((error) => {
             console.error("Error getting data: ", error);
         });
-
 }
 
+// Adds 1 like to an item in the DB
+function addLikeInDB(item, currentLikes) {
+    const newLikes = currentLikes + 1
+    // Update the like count
+    const updates = { likes: newLikes }
+    update(item, updates)
+        .then(() => {
+            console.log("Data updated successfully.");
+        })
+        .catch((error) => {
+            console.error("Error updating data: ", error);
+        });
+}
 
-
+// Removes 1 like to an item in the DB
+function removeLikeInDB(item, currentLikes) {
+    const newLikes = currentLikes - 1
+    // Update the like count
+    const updates = { likes: newLikes }
+    update(item, updates)
+        .then(() => {
+            console.log("Data updated successfully.");
+        })
+        .catch((error) => {
+            console.error("Error updating data: ", error);
+        });
+}
 
 publishBtn.addEventListener('click', publish)
 
+// Render the endorsement feed on any DB change
+onValue(endorsementsInDB, function (snapshot) {
+    if (snapshot.exists()) {
+        clearEndorsements()
+        let endorsementsArray = Object.entries(snapshot.val())
+        // console.log(endorsementsArray)
+        listEndorsements(endorsementsArray)
+    } else {
+        const text = document.createElement("p")
+        text.innerText = "No endorsements here yet"
+        endorsements.append(text)
+    }
+})
