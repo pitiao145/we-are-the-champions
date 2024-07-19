190cafca887a7bba15fd7f0f86cba03a1a63da3b
pitiao145 <pierre.bruyninckx1@gmail.com>
2024-07-19 08:14:52 +0800

    Styling, clear functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove, get, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove, get, update, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 // Configure the database
 const appSettings = {
@@ -15,6 +15,7 @@ const fromInput = document.getElementById("from-input")
 const toInput = document.getElementById("to-input")
 const publishBtn = document.getElementById("publish-btn")
 const endorsements = document.getElementById("endorsements")
+const clearBtn = document.getElementById("clear-btn")
 // Functions
 //Define an endorsement object
 function Endorsement(text, from, to) {
@@ -52,13 +53,14 @@ function publish() {
     clearInputs()
 }
 
-function createEndorsementBox(id) {
+function createEndorsementBox(id, delay) {
     const endorsementBox = document.createElement("div")
     endorsementBox.className = "endorsement-box"
     endorsementBox.id = id
     endorsementBox.addEventListener('dblclick', function () {
         handleLike(id)
     })
+    endorsementBox.style.animationDelay = `${delay}s`; // Set animation delay
     return endorsementBox
 }
 
@@ -84,8 +86,8 @@ function createFromToHeartSpan(toFromHeart, name, likes) {
 }
 
 // To create a visual for an endorsment pulled from DB.
-function createEndorsement(id, from, to, text, likes) {
-    const endorsementBox = createEndorsementBox(id)
+function createEndorsement(id, from, to, text, likes, delay) {
+    const endorsementBox = createEndorsementBox(id, delay)
     const endorsementText = createEndorsementText(text)
     const toSpan = createFromToHeartSpan("to", to)
     const fromSpan = createFromToHeartSpan("from", from)
@@ -110,7 +112,8 @@ function listEndorsements(array) {
         // console.log("Text: " + endorsementText)
         // console.log("Likes: " + endorsementLikes)
         // console.log("Key: " + endorsementKey + "| Value: " + endorsementValue)
-        createEndorsement(endorsementID, endorsementFrom, endorsementTo, endorsementText, endorsementLikes)
+        const delay = i * 0.2
+        createEndorsement(endorsementID, endorsementFrom, endorsementTo, endorsementText, endorsementLikes, delay)
     }
 }
 
@@ -169,7 +172,23 @@ function removeLikeInDB(item, currentLikes) {
         });
 }
 
-publishBtn.addEventListener('click', publish)
+function clearAllEndorsementsInDB() {
+    clearEndorsements()
+    set(endorsementsInDB, null)
+}
+
+publishBtn.addEventListener('click', function () {
+    if (endorsementInput.value === "" || fromInput.value === "" || toInput.value === "") {
+        alert("Please fill in all the fields")
+    } else {
+        publish()
+    }
+})
+
+clearBtn.addEventListener('click', function(snapshot) {
+    clearAllEndorsementsInDB()
+    console.log("Database cleared")
+})
 
 // Render the endorsement feed on any DB change
 onValue(endorsementsInDB, function (snapshot) {
@@ -178,9 +197,11 @@ onValue(endorsementsInDB, function (snapshot) {
         let endorsementsArray = Object.entries(snapshot.val())
         // console.log(endorsementsArray)
         listEndorsements(endorsementsArray)
+        clearBtn.style.display = "block"
     } else {
         const text = document.createElement("p")
         text.innerText = "No endorsements here yet"
         endorsements.append(text)
+        clearBtn.style.display = "none"
     }
 })
